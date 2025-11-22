15b2eadbeadfef7e4f6a668ff74f79896a8bd60d
Bli_Aik <haikun2333@126.com>
2025-11-22 17:48:01 +0800

    feat(mortar): add string variable for localized text in control flow

--- examples/dialogue_ui.rs
+++ examples/dialogue_ui.rs
@@ -56,11 +56,11 @@ impl Default for DialogueFiles {
     fn default() -> Self {
         Self {
             files: vec![
+                "control_flow.mortar".to_string(),
                 "branch_interpolation.mortar".to_string(),
                 "simple.mortar".to_string(),
                 "demo.mortar".to_string(),
                 "basic.mortar".to_string(),
-                "control_flow.mortar".to_string(),
                 "performance_system.mortar".to_string(),
             ],
             current_index: 0,
